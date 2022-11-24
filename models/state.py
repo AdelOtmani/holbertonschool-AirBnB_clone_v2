459cf8959708f6efb212ae0877c76d907f6e4844
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-24 22:32:53 +0100

    Task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states" 
+    name = Column(String(128), nullable=False)
