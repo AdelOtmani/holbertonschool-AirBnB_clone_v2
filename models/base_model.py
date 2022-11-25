664a0d05d6d817b497e150f67fdf98f243735ca9
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-25 15:29:33 +0100

    Task 6  basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -17,17 +17,18 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
+        
+        if (not kwargs) or ("created_at" and "updated_at" not in kwargs):    
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
