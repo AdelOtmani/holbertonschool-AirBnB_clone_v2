6c7c81136eaf7bcfc5e5a5b44b2c9d7c997b8124
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-25 15:33:41 +0100

    Task 6  basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         
-        if (not kwargs) or ("created_at" and "updated_at" not in kwargs):    
+        if (not kwargs) or ("created_at" or "updated_at" not in kwargs):    
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
