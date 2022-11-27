7440505ab0cee12b5c65863710fe907b51608b73
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-27 23:07:29 +0100

    Task 9

--- models/place.py
+++ models/place.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import getenv
+
+from sqlalchemy.orm import relationship
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import String, Integer, Float
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
+
+from models.review import Review
 
 
 class Place(BaseModel):
@@ -21,3 +27,21 @@ class Place(BaseModel):
     latitude =  Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+
+
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def review(self):
+            """ getter attribute reviews that returns
+                the list of Review instances with place_id equals 
+                to the current Place.id
+                => It will be the FileStorage relationship between 
+                Place and Review
+            """
+            list_review = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    list_review.append(review)
+            return(list_review)
