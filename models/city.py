599a9f5ef1448b23cecdc3de9a08b1d591dc7386
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-27 22:38:11 +0100

    Task 8 relationship places

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import Base
 from sqlalchemy import String
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,4 +13,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    
+    places = relationship("Place",backref="cites", cascade="all, delete")
