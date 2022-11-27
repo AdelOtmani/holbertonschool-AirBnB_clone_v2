8750abc6f9b5a2ab0d26afe7bce2210add284af7
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2022-11-27 22:17:54 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import String
 from sqlalchemy import Column
-from sqlalchemy.orm import relationship
+
 
 
 class User(BaseModel, Base):
