82e0746e2464346f5434c6a13eff249c7d87ec81
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-02 18:02:00 +0100

    task 6 int:n flask syntaxe

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,7 +48,7 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """ display n """
     return render_template("6-number.html", n=n)
@@ -56,4 +56,3 @@ def number_odd_or_even(n):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-    
