9940749d985d73db20cc7df8e12b3e4202f8ee00
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-02 15:42:28 +0100

    task 0 pep8 validation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,9 +9,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
+
 def Hello_HBNB():
     """ display Hello HBNB  """
-    return  "Hello HBNB!"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
