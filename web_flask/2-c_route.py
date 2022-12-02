2ad888d53efc5f96037d52575417dc7972c4e64b
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-02 17:40:42 +0100

    task 2 pep8 validation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,10 +14,12 @@ def Hello_HBNB():
     """ display Hello HBNB  """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """ display HBNB  """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def C(text):
